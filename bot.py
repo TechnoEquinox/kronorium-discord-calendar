774a2654f99e40066238a02c7f25a16ab6a19e70
TechnoEquinox <26304831+TechnoEquinox@users.noreply.github.com>
2024-07-17 13:29:31 -0400

    Added help text and customized help response

--- bot.py
+++ bot.py
@@ -16,11 +16,44 @@ CHANNEL_ID = int(auth['channel_id'])
 # Load events from kronorium.json
 events = helper.load_kronorium()
 
+# Loads the config from config.json
+config = helper.load_config()
+
 # Set up the bot with the command prefix 'kron!' and intents
 intents = discord.Intents.default()
 intents.message_content = True  # Enables access to message content
 
 bot = commands.Bot(command_prefix='kron!', intents=intents)
+
+class CustomHelpCommand(commands.HelpCommand):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+    async def send_bot_help(self, mapping):
+        ctx = self.context
+        response = f"Kronorium Discord Calendar\nVersion: {self.config['version']}\nCreated by: TechnoEquinox\n\n"
+        response += "Commands:\n"
+        for cog, commands in mapping.items():
+            for command in commands:
+                response += f"{command.name} - {command.help}\n"
+        await ctx.send(f"```{response}```")
+
+    async def send_command_help(self, command):
+        ctx = self.context
+        response = f"Help for command: {command.name}\n\n"
+        response += f"{command.help}\n\n"
+        response += f"Usage: {self.clean_prefix}{command.name} {command.signature}"
+        await ctx.send(f"```{response}```")
+
+    async def send_cog_help(self, cog):
+        ctx = self.context
+        response = f"Help for category: {cog.qualified_name}\n\n"
+        for command in cog.get_commands():
+            response += f"{command.name} - {command.help}\n"
+        await ctx.send(f"```{response}```")
+
+bot.help_command = CustomHelpCommand(config)
 
 @bot.event
 async def on_ready():
@@ -30,7 +63,7 @@ async def on_ready():
         for channel in guild.channels:
             print(f'- - Channel: {channel.name} (id: {channel.id})')
 
-@bot.command()
+@bot.command(help="Ping the bot to check if it's online.")
 async def ping(ctx):
     print(f'Received ping command in channel {ctx.channel.id}')
     
@@ -39,7 +72,7 @@ async def ping(ctx):
     else:
         print(f'Ping command ignored in channel {ctx.channel.id}')
 
-@bot.command()
+@bot.command(help="Get the events for today in Call of Duty Zombies history.")
 async def today(ctx):
     print(f'Received today command in channel {ctx.channel.id}')
     
@@ -49,7 +82,7 @@ async def today(ctx):
     else:
         print(f'Today command ignored in channel {ctx.channel.id}')
 
-@bot.command()
+@bot.command(help="Show the configuration of the bot.")
 async def config(ctx):
     print(f'Received config command in channel {ctx.channel.id}')
 
